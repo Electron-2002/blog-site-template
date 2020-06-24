a797abb121872e644da434bcc6a30d3834e72fda
Electron-2002 <satej.bidvai@gmail.com>
2020-06-24 16:36:40 +0530

    Updates

---
+++ precache-manifest.8ff4e3b271c8afe61553db33a35eaa46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75eada479bdfa0518dc806d10687d7d7",
+    "url": "/blog-site-template/index.html"
+  },
+  {
+    "revision": "fdd16da4c50c1f1abe15",
+    "url": "/blog-site-template/static/css/main.92597897.chunk.css"
+  },
+  {
+    "revision": "c928430afb7869dbe094",
+    "url": "/blog-site-template/static/js/2.a3953135.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/blog-site-template/static/js/2.a3953135.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdd16da4c50c1f1abe15",
+    "url": "/blog-site-template/static/js/main.c3577245.chunk.js"
+  },
+  {
+    "revision": "ccd3a4b3c97245236fc1",
+    "url": "/blog-site-template/static/js/runtime-main.07ead886.js"
+  }
+]);
